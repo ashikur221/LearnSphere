b8c426fbbf11d5eb363753bc49be07b9e6ace9a9
ashikur221 <ashikur1603@gmail.com>
2025-09-14 14:22:05 +0600

    working on mobile navbar

--- src/shared/navbar/MobileNavbar.jsx
+++ src/shared/navbar/MobileNavbar.jsx
@@ -71,43 +71,13 @@ const MobileNavbar = () => {
                 {/* Navigation Links */}
                 {GlobalNavLinks.map((item, index) => (
                     <div key={index}>
-                        {item.subcategories ? (
-                            <div className="relative">
-                                <button
-                                    onClick={() => setBuyDropdownOpen(!buyDropdownOpen)}
-                                    className="flex items-center justify-between w-full py-2 text-lg !text-gray-800"
-                                >
-                                    {item.name}
-                                    <ChevronDown
-                                        size={20}
-                                        className={`transition-transform duration-200 ${buyDropdownOpen ? "rotate-180" : ""
-                                            }`}
-                                    />
-                                </button>
-                                {buyDropdownOpen && (
-                                    <div className="ml-4 border-l border-gray-300 pl-4">
-                                        {item.subcategories.map((subItem, subIndex) => (
-                                            <NavLink
-                                                key={subIndex}
-                                                to={`${subItem.link}/${subItem.name}`}
-                                                className="block py-2 text-base !text-gray-600 hover:text-gray-900 hover:font-semibold"
-                                                onClick={handleNavLinkClick}
-                                            >
-                                                {subItem.name}
-                                            </NavLink>
-                                        ))}
-                                    </div>
-                                )}
-                            </div>
-                        ) : (
-                            <NavLink
-                                to={item.link}
-                                className="block py-2 text-lg !text-gray-800 hover:font-semibold"
-                                onClick={handleNavLinkClick}
-                            >
-                                {item.name}
-                            </NavLink>
-                        )}
+                        <NavLink
+                            to={item.link}
+                            className="block py-2 text-lg !text-gray-800 hover:font-semibold"
+                            onClick={handleNavLinkClick}
+                        >
+                            {item.name}
+                        </NavLink>
                     </div>
                 ))}
             </Drawer>
