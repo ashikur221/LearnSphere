0ac33052e26c71d0abca2343adbac2b7201b9e52
najmul890890890 <najmul890890890@gmail.com>
2025-09-14 11:20:59 +0600

    folder set up done

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Footer from "@/shared/footer/Footer";
+import Navbar from "@/shared/navbar/Navbar";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
