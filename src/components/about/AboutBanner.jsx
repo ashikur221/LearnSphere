7bc0ddbd4ab2a64443fa3d3f4e8899360625262e
ashikur221 <ashikur1603@gmail.com>
2025-09-30 16:10:49 +0600

    some changes on about

--- src/components/about/AboutBanner.jsx
+++ src/components/about/AboutBanner.jsx
@@ -1,4 +1,6 @@
 import { ImageProvider } from '@/utils/ImageProvider';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import React, { useEffect, useRef } from 'react';
 
 const AboutBanner = () => {
@@ -6,7 +8,35 @@ const AboutBanner = () => {
     const titleRef = useRef(null);
     const descRef = useRef(null);
 
-    useEffect(()=>{}, [])
+    useEffect(() => {
+        gsap.fromTo(
+            titleRef.current,
+            {
+                opacity: 0,
+                y: 50,
+            },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1.5,
+                ease: "power3.out",
+            }
+        )
+        
+        gsap.fromTo(
+            descRef.current,
+            {
+                opacity: 0,
+                y: 50,
+            },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1.5,
+                ease: "power3.out",
+            }
+        )
+    }, [])
 
     return (
         <div>
@@ -23,10 +53,10 @@ const AboutBanner = () => {
                 {/* Content Overlay */}
                 <div className="absolute inset-0 flex flex-col lg:flex-row items-center justify-center md:justify-start gap-6 md:gap-8 p-4 md:p-8">
                     <div className="w-full max-w-4xl text-center lg:text-start space-y-5">
-                        <p className="text-2xl text-white font-bold md:text-4xl lg:text-5xl">
+                        <p ref={titleRef} className="text-2xl text-white font-bold md:text-4xl lg:text-5xl">
                             Who We Are & What We Do
                         </p>
-                        <p className="text-sm mt-2 md:mt-4 md:text-base lg:text-xl text-white">
+                        <p ref={descRef} className="text-sm mt-2 md:mt-4 md:text-base lg:text-xl text-white">
                             We provide accessible and affordable online training to help you become a Certified RBT. Our 40-hour course is designed to meet BACB® requirements and prepare you with the skills and confidence to succeed.
                         </p>
                         <button className='bg-theme-primary text-white py-3 px-5 rounded-full'>Start Training</button>
